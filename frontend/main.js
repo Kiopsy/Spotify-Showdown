8f25ca203e293af3d0165cb020594e1472fd779d
Nathan Malicki <43974544+ntm5579@users.noreply.github.com>
2022-01-05 02:52:51 -0500

    Add files via upload

--- frontend/main.js
+++ frontend/main.js
@@ -13,3 +13,20 @@ function settings(){
 function profile(){
     alert("Testing profile")
 }
+
+//https://code.tutsplus.com/tutorials/how-to-create-a-simple-web-based-chat-application--net-5931
+function send(){
+    var s = document.getElementById("chatinput").value;
+    document.getElementById("chatinput").value = ""; //resets the chat box text
+
+    const d = new Date();
+    var half = "am";
+    if(d.getHours() > 11){
+        half = "pm";
+    }
+    time = "" + (d.getHours() % 12) + ':' + d.getMinutes() +" " + half;
+    //time += " - " + (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear()
+     
+    document.getElementById("chatoutput").innerHTML += "\n" + s + " - " + time; //newline does not work as expected
+    //find way to make timestamp dynamic, if it from same day, only list the time not the date
+}
