ccb17815812deeb20327e1bd137f257c7cd1ef7f
Nathan Malicki <43974544+ntm5579@users.noreply.github.com>
2022-01-04 19:41:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function alertTest(){
+    alert("Testing alert!")
+}
+
+function mute(){
+    alert("Testing mute")
+}
+
+function settings(){
+    alert("Testing settings")
+}
+
+function profile(){
+    alert("Testing profile")
+}
